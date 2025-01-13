ec58cd2774c759afabf23af879b266a3aa40dfba
chengfeixun <101888176+chengfeixun@users.noreply.github.com>
2025-01-13 17:10:56 +0800

    Update OP61a.sh

--- OP61a.sh
+++ OP61a.sh
@@ -35,7 +35,7 @@ rm -rf package/custom; mkdir package/custom
 # Add a feed source
 git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/luci-theme-argon-18.06
-# git clone https://github.com/kenzok78/luci-app-fileassistant.git package/luci-app-fileassistant
+git clone https://github.com/kenzok78/luci-app-fileassistant.git package/luci-app-fileassistant
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 # sed -i '$a src-git jerryk https://github.com/jerrykuku/openwrt-package' feeds.conf.default
 # git clone https://github.com/zzsj0928/luci-app-pushbot package/luci-app-pushbot
